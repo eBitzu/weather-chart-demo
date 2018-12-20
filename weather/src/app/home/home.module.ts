eb64e9e2dc8bd714d929e579d9c9a127f736303b
Gabriel Raducu <raducu.gabriel@gmail.com>
2018-12-20 14:48:49 +0200

    Adding some 'glue'

--- weather/src/app/home/home.module.ts
+++ weather/src/app/home/home.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyMaterialModule } from './material.module';
 import { ChartsModule } from 'ng2-charts';
+import * as fromComponents from './components'
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     MyMaterialModule,
-    ChartsModule
+    ChartsModule,
+    ReactiveFormsModule,
   ],
-  declarations: []
+  declarations: [...fromComponents.components],
+  exports: [...fromComponents.components, ReactiveFormsModule ],
 })
 export class HomeModule { }
--- weather/src/environments/environment.prod.ts
+++ weather/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API_ENDPOINT: 'https://s3.eu-west-2.amazonaws.com/interview-question-data/',
 };
